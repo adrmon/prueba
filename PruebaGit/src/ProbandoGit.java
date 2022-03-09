302b58872e859a8f10755e56410a9d6de50e201d
adrmon <vdrivn97@gmail.com>
2022-03-09 13:02:26 +0100

    Nueva versión He añadido cosas

--- PruebaGit/src/ProbandoGit.java
+++ PruebaGit/src/ProbandoGit.java
@@ -4,6 +4,8 @@ public class ProbandoGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hola Mundo!");
+System.out.println("Que tal");
+System.out.println("Probando GitHub");
 	}
 
 }
